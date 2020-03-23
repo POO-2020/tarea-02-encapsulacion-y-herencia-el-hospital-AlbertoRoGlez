00f34b47d0dc8f7f3d57cc9680237986493aff2d
Alberto Rosales <arosales7@ucol.mx>
2020-03-22 19:37:36 -0600

    4

--- hospital.js
+++ hospital.js
@@ -1,6 +1,6 @@
 import Fecha from './fecha.js';
-import Tiempo from './Tiempo.js';
-import Nombre from './Nombre.js';
+import Tiempo from './tiempo.js';
+import Nombre from './nombre.js';
 import Paciente from './paciente.js';
 import Doctor from './doctor.js';
 import Cita from './cita.js';
@@ -11,28 +11,28 @@ export default class Hospital {
    * @param {string} direccion
    */
   constructor(nombre, direccion) {
-    this.nombre = nombre;
-    this.direccion = direccion;
-    this.doctores = new Array();
-    this.citas = new Array();
+    this._nombre = nombre;
+    this._direccion = direccion;
+    this._doctores = new Array();
+    this._citas = new Array();
   }
 
   registrarDoctor(doctor) {
-    this.doctores.push(doctor);
+    this._doctores.push(doctor);
   }
 
   listarDoctores() {
-    this.doctores.forEach((doctor, i) => {
+    this._doctores.forEach((doctor, i) => {
       console.log(`${i + 1} ${doctor.getPerfil()}`);
     });
   }
 
   registrarCita(cita) {
-    this.citas.push(cita);
+    this._citas.push(cita);
   }
 
   listarCitas() {
-    this.citas.forEach((cita, i) => {
+    this._citas.forEach((cita, i) => {
       console.log(`${i + 1} ${cita.getCita()}`);
     });
   }
