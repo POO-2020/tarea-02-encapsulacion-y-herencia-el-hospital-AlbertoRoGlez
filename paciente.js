52f7bebdbe2956570fdf169025db9bbbd03be2eb
Alberto Rosales <arosales7@ucol.mx>
2020-03-22 19:39:04 -0600

    6

--- paciente.js
+++ paciente.js
@@ -8,14 +8,14 @@ export default class Pacientes {
    * @param {number} numeroTel
    */
   constructor(nombre, fechaNacimiento, telefono) {
-    this.nombre = nombre;
-    this.fechaNacimiento = fechaNacimiento;
-    this.telefono = telefono;
+    this._nombre = nombre;
+    this._fechaNacimiento = fechaNacimiento;
+    this._telefono = telefono;
   }
 
   getPerfil() {
-    return `${this.nombre.getNombreCompleto()}, ${this.fechaNacimiento.getFecha()}, ${
-      this.telefono
+    return `${this._nombre.getNombreCompleto()}, ${this._fechaNacimiento.getFecha()}, ${
+      this._telefono
     }`;
   }
 }
